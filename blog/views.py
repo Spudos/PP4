1d5a9f05b3b8eb63c6bfc4a283e884e4654050ae
Andy Pierce <spudos16@gmail.com>
2024-05-10 11:01:17 +0100

    add comment view

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic, View
 from .models import Post
+from .forms import CommentForm
 
 class BlogEntries(generic.ListView):
   model = Post
@@ -23,6 +24,39 @@ class BlogDetail(View):
         {
             "blog": blog,
             "comments": comments,
-            "liked": liked
+            "commented": False,
+            "liked": liked,
+            "comment_form": CommentForm()
         },
-    )
+  )
+    
+  def post(self, request, slug, *args, **kwargs):
+    queryset = Post.objects.filter(status=1)
+    blog = get_object_or_404(queryset, slug=slug)
+    comments = blog.comments.filter(approved=True).order_by("-created_on")
+    liked = False
+    if blog.likes.filter(id=self.request.user.id).exists():
+        liked = True
+
+    comment_form = CommentForm(data=request.POST)
+    
+    if comment_form.is_valid():
+      comment_form.instance.email = request.user.email
+      comment_form.instance.name = request.user.username
+      comment = comment_form.save(commit=False)
+      comment.post = blog
+      comment.save()
+    else:
+      comment_form = CommentForm()
+    
+    return render(
+        request,
+        "blog_detail.html",
+        {
+            "blog": blog,
+            "comments": comments,
+            "commented": True,
+            "liked": liked,
+            "comment_form": CommentForm()
+        },
+  )
