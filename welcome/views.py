b25163e995fc92f2f3c9d2b1d4463ca0e99067ad
Andy Pierce <spudos16@gmail.com>
2024-05-14 13:11:00 +0100

    reorder blog header page

--- welcome/views.py
+++ welcome/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from blog.models import Post
 
-# Create your views here.
 def welcome(request):
-  return render(request, 'welcome.html')
+    blog_entries = Post.objects.filter(status=1).order_by('-created_on')
+    latest_blog_entry = blog_entries.first()
+    selected_entries = blog_entries[1:4]
+    
+    return render(request, 'welcome.html', {'latest_blog_entry': latest_blog_entry, 'selected_entries': selected_entries})
+
