8d3cc23165ff6a7d3252429d18c92d02c0227e61
Andy Pierce <spudos16@gmail.com>
2024-05-15 17:31:45 +0100

    add booking and session models and start welcome view

--- booking/models.py
+++ booking/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+TYPE = ((0, "Gym"),
+        (1, "Strength"),
+        (2, "Cardio"),
+        (3, "Nutrition"),
+        (4, "Motivation"),
+        (5, "Weight Loss"))
+
+class Booking(models.Model):
+  type = models.IntegerField(choices=TYPE, default=0)
+  notes = models.TextField()
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
+  cancelled = models.BooleanField(default=False)
+  booking_date_time = models.DateTimeField()
+  last_update = models.DateTimeField(auto_now=True)
+  created_on = models.DateTimeField(auto_now_add=True)
+  
+class Sessions(models.Model):
+  type = models.IntegerField(choices=TYPE, default=0)
+  Description = models.TextField(max_length=200, unique=True)
+  excerpt = models.TextField(max_length=50, unique=True)
+  image = CloudinaryField('image', default='none')
+  last_update = models.DateTimeField(auto_now=True)
+  created_on = models.DateTimeField(auto_now_add=True)
+
--- welcome/views.py
+++ welcome/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from blog.models import Post
+from booking.models import Sessions
 
 def welcome(request):
     blog_entries = Post.objects.filter(status=1).order_by('-created_on')
     latest_blog_entry = blog_entries.first()
     selected_entries = blog_entries[1:4]
     
-    return render(request, 'welcome.html', {'latest_blog_entry': latest_blog_entry, 'selected_entries': selected_entries})
+    session_types = Sessions.objects.all()
+    sessions_a = session_types[0:3]
+    sessions_b = session_types[3:6]
+    
+    return render(request, 'welcome.html', {'latest_blog_entry': latest_blog_entry, 'selected_entries': selected_entries, 'sessions_a': sessions_a, 'sessions_b': sessions_b})
 
